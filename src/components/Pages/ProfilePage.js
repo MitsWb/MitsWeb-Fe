4dad05bb00b955dd712d21d61a55ddde42aeda28
unknown <psjishnu13@gmail.com>
2020-12-12 15:04:39 +0530

    added profile/

--- src/components/Pages/ProfilePage.js
+++ src/components/Pages/ProfilePage.js
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
+import { Button, Grid } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import UseHeading from "./useHeading";
 import { Avatar } from "@material-ui/core";
@@ -15,7 +15,7 @@ import BackButton from "../buttons/BackButton";
 const useStyles = makeStyles({
   root: {
     margin: "0px auto",
-    textAlign: "center",
+    //   textAlign: "center",
     maxWidth: "500px",
   },
   media: {
@@ -29,6 +29,13 @@ const useStyles = makeStyles({
   },
   email: {
     fontSize: 15,
+  },
+  parent: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  title: {
+    fontWeight: "bold",
   },
 });
 
@@ -48,19 +55,78 @@ const ProfilePage = () => {
           src={currentUser.photo ? currentUser.photo : noImg}
         ></Avatar>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
+          <Typography
+            className="text-center"
+            gutterBottom
+            variant="h5"
+            component="h2"
+          >
             {currentUser.name}
           </Typography>
-          <Typography gutterBottom className={classes.number}>
-            <PhoneIcon fontSize="small" /> {currentUser.number}
-          </Typography>
-          <Typography gutterBottom className={classes.email}>
-            {currentUser.email}
-          </Typography>
+          <Grid container spacing={2} className="text-center">
+            <Grid item xs={12} sm={6}>
+              <Typography gutterBottom className="text-center lg:text-right">
+                <PhoneIcon fontSize="small" /> {currentUser.number}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography gutterBottom className="text-center lg:text-left">
+                {currentUser.email}
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Card className="p-1 bg-red-200">
+            <CardContent>
+              <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
+                <Typography>
+                  <span className={classes.title}>D.O.B : </span> 01-Jan-2001
+                </Typography>
+                <Typography>
+                  <span className={classes.title}>Blood Group : </span> O+
+                </Typography>
+              </div>
+
+              <Typography>
+                <span className={classes.title}> Address :</span> Abc House,
+                Edappally, Ernakulam, PIN : 635124
+              </Typography>
+              <br></br>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Typography>
+                    <Typography className={classes.parent}>
+                      Details of Father :
+                    </Typography>
+                    <span className={classes.title}>Name :</span> Sreekumar P.N{" "}
+                    <br></br>
+                    <span className={classes.title}>Mobile No : </span>
+                    9874514878
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Typography>
+                    <Typography className={classes.parent}>
+                      Details of Mother :
+                    </Typography>
+                    <span className={classes.title}>Name :</span> Bindu .N{" "}
+                    <br></br>
+                    <span className={classes.title}>Mobile No : </span>
+                    9854510124
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
         </CardContent>
         <CardActions>
           <A href="/updateprofile">
-            <Button size="small" color="primary" style={{ outline: "none" }}>
+            <Button
+              size="small"
+              variant="contained"
+              color="primary"
+              style={{ outline: "none" }}
+            >
               Edit profile
             </Button>
           </A>
