55adb6a79ef93745f33667d7c9eb33449b3e4cb9
Arihant Hirawat <arihant2310@gmail.com>
2021-04-13 17:59:52 +0530

    Modifies add button component

--- src/components/buttons/AddButton.js
+++ src/components/buttons/AddButton.js
@@ -1,33 +1,22 @@
 import React from "react";
-import { Button, makeStyles } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import { A } from "hookrouter";
-const useStyles = makeStyles((theme) => ({
-  link: {
-    textDecoration: "none",
-    color: "inherit",
-    cursor: "pointer",
-  },
-  container: {
-    marginTop: "10px",
-  },
-}));
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+
 const Addbutton = ({ title, href }) => {
-  const classes = useStyles();
   return (
-    <Button variant="outlined" color="primary" style={{ outline: "none" }}>
-      <A
-        href={href}
-        className={classes.link}
-        style={{
-          color: "#757de8",
-          fontSize: "16px",
-          textDecoration: "none",
-          outline: "none",
-        }}
-      >
-        {title}
+    <div className="w-full text-center pb-2">
+      <A href={href}>
+        <Button
+          className="mb-2"
+          variant="contained"
+          color="primary"
+          startIcon={<AddCircleIcon />}
+        >
+          {title}
+        </Button>
       </A>
-    </Button>
+    </div>
   );
 };
 export default Addbutton;
