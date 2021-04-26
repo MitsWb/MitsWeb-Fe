2da9e417935068f817f0d98ce34a14ab033332a5
Arihant Hirawat <arihant2310@gmail.com>
2021-04-27 01:07:05 +0530

    🎉 Add navbar to semester dashboard

---
+++ src/components/Departments/Marks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Marks() {
+  return (
+    <div>
+      <h1>Marks should be uploaded here!!</h1>
+    </div>
+  );
+}
+
+export default Marks;
--- src/components/Departments/SemesterInformation.js
+++ src/components/Departments/SemesterInformation.js
@@ -1,11 +1,54 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import RestoreIcon from "@material-ui/icons/Restore";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
 import useHeading from "../Pages/Shared/useHeading";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import Marks from "./Marks";
+
+const useStyles = makeStyles({
+  root: {
+    width: "auto",
+    marginBottom: 10,
+  },
+});
 
 function SemesterInformation({ department, semester }) {
   useHeading(`${department.toUpperCase()} ${semester.toUpperCase()} Dashboard`);
+  const classes = useStyles();
+  const [value, setValue] = React.useState("recents");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <>
-      <h1>semester information need to be displayed here</h1>
+      <BottomNavigation
+        value={value}
+        onChange={handleChange}
+        className={classes.root}
+        showLabels
+      >
+        <BottomNavigationAction
+          label="Discussions"
+          value="discussions"
+          icon={<RestoreIcon />}
+        />
+
+        <BottomNavigationAction
+          label="Marks"
+          value="marks"
+          icon={<LibraryBooksIcon />}
+        />
+        <BottomNavigationAction
+          label="Notes"
+          value="notes"
+          icon={<MenuBookIcon />}
+        />
+      </BottomNavigation>
+      {value === "marks" && <Marks />}
     </>
   );
 }
