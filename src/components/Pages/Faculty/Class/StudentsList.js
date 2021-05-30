ee4a8b6fb0f446dad8c3a4f432b40b153ffcc4b9
Jishnu P Sreekumar <psjishnu13@gmail.com>
2021-05-30 13:38:50 +0530

    Faculty view attendance

---
+++ src/components/Pages/Faculty/Class/StudentsList.js
@@ -0,0 +1,128 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Dialog from "@material-ui/core/Dialog";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import { Typography, Card, CardContent, Grid } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import Slide from "@material-ui/core/Slide";
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: "fixed",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function EnterMarksDialog({ open, handleClose, data, list }) {
+  const classes = useStyles();
+  return (
+    <>
+      <div>
+        <Dialog
+          fullScreen
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Transition}
+        >
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={handleClose}
+                aria-label="close"
+              >
+                <CloseIcon />
+              </IconButton>
+              <Grid
+                style={{ width: "100%" }}
+                container
+                xs={6}
+                sm={12}
+                spacing={2}
+              >
+                <Grid item>
+                  <div className="truncate">
+                    <Typography>
+                      {`${data.startTime}-${data.endTime}`}
+                    </Typography>
+                  </div>
+                </Grid>
+                <Grid item>
+                  <div className="truncate">
+                    <Typography> {data.timeStamp}</Typography>
+                  </div>
+                </Grid>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+          <div style={{ marginTop: "60px" }}>
+            {list.map((value, index) => {
+              return (
+                <div
+                  style={{
+                    height: "200",
+                    marginTop: "6px",
+                    marginBottom: "6px",
+                  }}
+                >
+                  <Card
+                    key={index}
+                    style={{
+                      margin: "0px auto",
+                      minWidth: "300px ",
+                      maxWidth: "500px",
+                    }}
+                  >
+                    <CardContent>
+                      <div className="flex relative flex-row">
+                        <div className="w-4/5">
+                          <Grid
+                            style={{ width: "100%" }}
+                            container
+                            xs={6}
+                            sm={12}
+                            spacing={2}
+                          >
+                            <Grid item>
+                              <div className="truncate">
+                                <Typography> {value.studentId}</Typography>
+                              </div>
+                            </Grid>
+                            <Grid item>
+                              <div className="truncate">
+                                <Typography> {value.name}</Typography>
+                              </div>
+                            </Grid>
+                          </Grid>
+                        </div>
+                        <div className="w-1/5">
+                          <Card
+                            style={{
+                              height: 30,
+                              width: 30,
+                              backgroundColor:
+                                value.present === "true" ? "green" : "red",
+                            }}
+                          ></Card>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              );
+            })}
+          </div>
+        </Dialog>
+      </div>
+    </>
+  );
+}
--- src/components/Pages/Faculty/Class/ViewAttendanceList.js
+++ src/components/Pages/Faculty/Class/ViewAttendanceList.js
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getAttendanceList } from "../../../../redux/apiActions";
-import { Notify } from "../../../../utils";
+import { Notify, CardSkeleton } from "../../../../utils";
+import { Grid, Card, Typography, CardContent } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import StudentList from "./StudentsList";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    cursor: "pointer",
+  },
+}));
 const ViewAttendance = ({ className }) => {
   const classDetails = className.split("-");
+  const classes = useStyles();
   const dispatch = useDispatch();
+  const [list, setlist] = useState([]);
+  const [data, setdata] = useState({ list: [], open: false, data: "" });
   const [notify, setnotify] = useState({ msg: "", popup: "", type: "" });
+  const [loading, setloading] = useState(false);
+
   useEffect(() => {
+    setloading(true);
     dispatch(
       getAttendanceList({
         semester: Number(classDetails[0][1]),
@@ -15,16 +35,49 @@ const ViewAttendance = ({ className }) => {
       })
     ).then((res) => {
       if (res && res.data && res.data.success) {
-        console.log(res.data.data);
+        setlist(res.data.data);
       } else if (res && res.data) {
         setnotify({ msg: res.data.msg || "Error", popup: true, type: "error" });
       }
+      setloading(false);
     });
-  }, [dispatch, classDetails]);
+    // eslint-disable-next-line
+  }, []);
+  const handleClick = (e) => {
+    setdata({ list: e.attendanceList, open: true, data: e });
+  };
   return (
     <>
+      <StudentList
+        open={data.open}
+        data={data.data}
+        list={data.list}
+        handleClose={() => setdata({ ...data, open: false })}
+      />
       <Notify props={notify} closeAlert={() => setnotify({ popup: false })} />
-      <div className="w-full text-center">Attendance List</div>
+      {loading ? (
+        <CardSkeleton height={120} />
+      ) : (
+        <Grid container spacing={3}>
+          {list.map((value, key) => {
+            return (
+              <Grid key={key} item xs={6} sm={3}>
+                <Card
+                  onClick={() => handleClick(value)}
+                  className={classes.paper}
+                >
+                  <CardContent>
+                    <Typography>{value.timeStamp}</Typography>
+                    <Typography>
+                      {value.startTime + "-" + value.endTime}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
     </>
   );
 };
